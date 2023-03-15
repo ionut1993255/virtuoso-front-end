4ae3c743b027dc44de4cd53ecc561be2d05cac07
ionut <ionut_ilisei@yahoo.com>
2023-03-15 21:17:05 +0200

    feat: mod in script.js file

--- script.js
+++ script.js
@@ -2,52 +2,58 @@ const pianoKeys = document.querySelectorAll(".piano-keys .key");
 volumeSlider = document.querySelector(".volume-slider input");
 keysCheckbox = document.querySelector(".keys-checkbox input");
 const instrumentSelector = document.querySelector('.instrument-selector');
+const displaySongs = document.querySelector('.show-song-list');
+const songList = document.querySelector('.song-list');
 
-let defaultPath = "/tunes/acoustic_grand_piano/";
+let defaultPath = "/tunes/acoustic_grand_piano/"; // By default the instrument is "Acoustic Grand Piano"
 
 let allKeys = [];
-let audio = new Audio(defaultPath); // by default, audio src is "a" tune
+let audio = new Audio(defaultPath);
 
 const playTune = (key) => {
-    audio.src = defaultPath + `${key}.wav`; // passing audio src based on key pressed 
-    audio.play(); // playing audio
+    audio.src = defaultPath + `${key}.wav`; // Passing audio src based on key pressed 
+    audio.play(); // Playing audio
 
-    const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
-    clickedKey.classList.add("active"); // adding active class to the clicked key element
-    setTimeout(() => { // removing active class after 150 ms from the clicked key element
+    const clickedKey = document.querySelector(`[data-key="${key}"]`); // Getting clicked key element
+    clickedKey.classList.add("active"); // Adding active class to the clicked key element
+    setTimeout(() => { // Removing active class after 150 ms from the clicked key element
         clickedKey.classList.remove("active");
     }, 150);
 }
 
 pianoKeys.forEach(key => {
-    allKeys.push(key.dataset.key); // adding data-key value to the allKeys array
-    // calling playTune function with passing data-key value as an argument
+    allKeys.push(key.dataset.key); // Adding data-key value to the allKeys array
+    // Calling playTune function with passing data-key value as an argument
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
 const handleVolume = (e) => {
-    audio.volume = e.target.value; // passing the range slider value as an audio volume
+    audio.volume = e.target.value; // Passing the range slider value as an audio volume
 }
 
 const showHideKeys = () => {
-    // toggling hide class from each key on the checkbox click
+    // Toggling hide class from each key on the checkbox click
     pianoKeys.forEach(key => key.classList.toggle("hide"));
 }
 
 const pressedKey = (e) => {
-    // if the pressed key is in the allKeys array, only call the playTune function
+    // If the pressed key is in the allKeys array, only call the playTune function
     if(allKeys.includes(e.key)) playTune(e.key);
 }
 
 const chosenInstrument = (e) => {
-    // chosen instrument based on selected input
+    // Chosen instrument based on selected input
     defaultPath = '/tunes/' + e.target.value + "/";
+}
+
+const displaySong = () => {
+    songList.classList.toggle('active'); 
+    // Toggling active class from songList when we click the button displaySongs
 }
 
 keysCheckbox.addEventListener("click", showHideKeys);
 volumeSlider.addEventListener("input", handleVolume);
 document.addEventListener("keydown", pressedKey);
 instrumentSelector.addEventListener('change', chosenInstrument);
-
-
+displaySongs.addEventListener('click', displaySong);
 
